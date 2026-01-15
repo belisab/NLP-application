70bd97927b66129fb461f3e5d1004fd8db08b609
Fiia Suomi <fiia.suomi@gmail.com>
2026-01-15 16:14:58 +0200

    #5 Url for Helsinki news

---
+++ task2.py
@@ -0,0 +1,15 @@
+# This file is for the task of extracting news headlines.
+
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# Website url
+url = "https://www.helsinki.fi/en/news/news-and-press-releases"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+print(soup)
+
